75138f621347aaf6917e62fd2e9a123f70c7e7c3
JuliaWeber <37297380+JuliaWeber@users.noreply.github.com>
2023-08-01 20:57:05 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
-use vanet4j::simulator::Simulator;
 use vanet4j::grid::GridParams;
-use vanet4j::rsu_manager::RsuManagerParams;
 use vanet4j::obu_manager::ObuManagerParams;
+use vanet4j::rsu_manager::RsuManagerParams;
+use vanet4j::simulator::Simulator;
 
 fn main() {
-
-    /**
-     * Parameters for experiments are adjusted here
-     */
-    
     let grid_params = GridParams {
         blocks_per_street: 25,
         block_size: 3,
     };
 
     let rsu_manager_params = RsuManagerParams {
-        comms_range: 5,
+        tx_range: 5, // how far can the RSU transmit?
+        rx_range: 5, // this will affect the spacing between RSUs
+        detect_obu_tx_failure: true,
+        detect_obu_gps_failure: false,
     };
 
     let obu_manager_params = ObuManagerParams {
         max_obus: 120,
-        comms_range: 2,
-        tx_base_failure_rate: 0.01,
+        comms_range: 6, // at least RSU rx_range + 1 
+        tx_base_failure_rate: 0.02,
         tx_faulty_obu_failure_rate: 0.05,
+        gps_failure_rate: 0.02, 
+        gps_faulty_obu_failure_rate: 0.05,
         faulty_obus: 20,
     };
 
@@ -31,5 +31,4 @@ fn main() {
     simulator.init();
 
     simulator.run(180);
-
 }
